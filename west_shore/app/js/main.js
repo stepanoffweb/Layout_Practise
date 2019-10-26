fdccc224b11abb9513ab10538b12efc78c437f6a
ivanstepanov <ivanstepanoff@yahoo.com>
2019-10-26 17:45:28 +0200

    the surf screen is done

--- west_shore/app/js/main.js
+++ west_shore/app/js/main.js
@@ -4,15 +4,30 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="previous item">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="next item">',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesTOScroll: 4,
         asNavFor: '.header__slider'
-    })
+    });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesTOScroll: 1,
+       prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="previous item">',
+       nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="next item">',
+       asNavFor: '.slider-map'
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesTOScroll: 1,
+        asNavFor: '.surf-slider',
+        arrows: false,
+        focusOnSelect: true
+    });
 
 
 });
