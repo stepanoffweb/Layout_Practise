280acf28fe6aa004f9c9694a222c1dc9f39ee334
ivanstepanov <ivanstepanoff@yahoo.com>
2019-10-26 18:49:21 +0200

    minor

---
+++ west_shore/app/js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="previous item">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="next item">',
+        asNavFor: '.slider-dotshead'
+    });
+
+    $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesTOScroll: 4,
+        asNavFor: '.header__slider'
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesTOScroll: 1,
+       prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="previous item">',
+       nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="next item">',
+       asNavFor: '.slider-map'
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesTOScroll: 1,
+        asNavFor: '.surf-slider',
+        arrows: false,
+        focusOnSelect: true
+    });
+
+
+});
